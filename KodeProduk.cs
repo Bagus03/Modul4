eaf4b382132114e14a0a0e58261881667980d18a
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-11 07:31:35 +0700

    Menambahkan Class kodeproduk

---
+++ KodeProduk.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul4_103022300025
+{
+    internal class KodeProduk
+    {
+        private Dictionary<string, string> tableKodeProduk = new Dictionary<string, string>()
+        {
+            { "laptop", "E100" },
+            { "smartphonr" , "E101" },
+            { "tablet", "E102" },
+            { "headset", "E103" },
+            { "keyboard", "E104" },
+            { "mouse", "E105" },
+            { "printer", "E106" },
+            { "monitor", "E107" },
+            { "smartwatch", "E108" },
+            { "kamera", "E109" },
+        };
+
+        public string? GetKodeProduk(string Produk)
+        {
+            if (tableKodeProduk.ContainsKey(Produk))
+            {
+                return tableKodeProduk[Produk];
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Modul4_103022300025
+{
+    class Program
+    {
+        static void Main()
+        {
+            KodeProduk kodeProduk = new KodeProduk();
+
+            Console.Write("Masukan nama Produk : ");
+            String Produk = Console.ReadLine();
+
+            string? KodeProdukResult = kodeProduk.GetKodeProduk(Produk);
+
+            if (KodeProdukResult != null)
+            {
+                Console.WriteLine($"Nama Produk untuk {Produk} adalah: {KodeProdukResult}");
+            }
+            else 
+            {
+                Console.WriteLine("Produk Tidak ditemukan.");
+            }
+        }
+    }
+}
